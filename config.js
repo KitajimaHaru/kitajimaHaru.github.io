a629dc71a324186f0fe45afab9cd042aad0fef57
Yuuzhi <95756692+KitajimaHaru@users.noreply.github.com>
2023-02-20 20:22:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var _config = {
 	//GitHub Api
     owner           : 'KitajimaHaru',           // github 用户名
     repo         	: 'StudyFiles',             // github 中对应项目名
-   // access_token    : 'ghp_kn7LJHo97uwhTfj'+'gmqr1T7WxGfVN1B3OvQne', // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
+    access_token    : 'ghp_kn7LJHo97uwhTfj'+'gmqr1T7WxGfVN1B3OvQne', // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
 	//Reading
     per_page        : '6',                    // 默认一页显示几篇文章
 	//theme
